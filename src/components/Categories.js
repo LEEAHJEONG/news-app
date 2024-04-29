b6f9f741782732c76d78c3850831647f5bbfec37
lha901222 <lha901222@gmail.com>
2024-04-29 10:51:11 +0900

    feat: 뉴스 외부 API 연동 실습 예제

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,49 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 import React from "react";
 import { NavLink } from "react-router-dom";
+
+const CategoriesStyle = styled.div`
+  display: flex;
+  padding: 1rem;
+  width: 768px;
+  margin: 0 auto;
+  gap: 15px;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+
+    // 반응형에서 스크롤바생성
+    overflow-x: auto;
+    // 또는
+    /* display: flex;
+    flex-wrap: wrap; */
+  }
+`;
+
+const NavLinkStyle = styled(NavLink)`
+  font-size: 1.125rem;
+  /* cursor: pointer; */
+  white-space: pre;
+  padding-bottom: 0.25rem;
+
+  &:hover {
+    color: #a5a5a5;
+  }
+
+  &.active {
+    font-weight: 600;
+    border-bottom: 2px solid #f4a4a4;
+    color: #f4a4a4;
+  }
+
+  /* ${props =>
+    props.active &&
+    css`
+      font-weight: 600;
+      border-bottom: 2px solid #f4a4a4;
+      color: #f4a4a4;
+    `} */
+`;
 
 const categories = [
   { name: "all", text: "전체보기" },
@@ -13,16 +57,17 @@ const categories = [
 
 const Categories = () => {
   return (
-    <div>
+    <CategoriesStyle>
       {categories.map(category => (
-        <NavLink
+        <NavLinkStyle
           key={category.name}
           to={category.name === "all" ? "/" : `/${category.name}`}
+          // className={({ isActive }) => (isActive ? "active" : undefined)}
         >
           {category.text}
-        </NavLink>
+        </NavLinkStyle>
       ))}
-    </div>
+    </CategoriesStyle>
   );
 };
 
